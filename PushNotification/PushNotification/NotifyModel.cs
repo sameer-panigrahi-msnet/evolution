a7482779cbea15954033e23ee5ee1e60d811f52a
Sameer <52082859+sameer-panigrahi-msnet@users.noreply.github.com>
2022-01-26 22:09:56 +0530

    Add files via upload

---
+++ PushNotification/PushNotification/NotifyModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PushNotification
+{
+   public class NotifyModel
+    {
+        public string Body { get; set; }
+        public string Title { get; set; }
+        public string IsSoundEnable { get; set; }
+    }
+}
